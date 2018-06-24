88c1f1457e5c1bf84e07d3a7922c1788a41c9fa2
ramasrirama99 <ramasrirama99@gmail.com>
2018-06-23 22:24:49 -0500

    Refactor db to remove foreign keys

--- set_up.sql
+++ set_up.sql
@@ -1,14 +1,3 @@
-CREATE TABLE IF NOT EXISTS tickers (
-    id    INTEGER PRIMARY KEY AUTOINCREMENT,
-    name    TEXT
-);
-
-CREATE TABLE IF NOT EXISTS dates (
-    id    INTEGER PRIMARY KEY AUTOINCREMENT,
-    date    TEXT
-);
-
-
 CREATE TABLE IF NOT EXISTS historical_data (
     date                INTEGER,
     ticker              INTEGER,
@@ -20,6 +9,4 @@ CREATE TABLE IF NOT EXISTS historical_data (
     volume              INTEGER,
     dividend_amount     REAL,
     split_coefficient   REAL,
-    FOREIGN KEY(date) REFERENCES dates(id),
-    FOREIGN KEY(ticker) REFERENCES tickers(id)
 );
