21253959c1452093a9c901a02c4f552b98c63200
Brian <byron123t@gmail.com>
2018-06-23 23:17:39 -0500

    Format api_interactor and create database.py

--- backend/api_interactor.py
+++ backend/api_interactor.py
@@ -3,26 +3,29 @@ import requests
 from io import StringIO
 import pandas
 import apikey
-#import config
+
+# import config
 
 API_URL = 'https://www.alphavantage.co/query'
 
-def single_csv(symbol, apikey, function = 'TIME_SERIES_DAILY_ADJUSTED', output = 'compact', datatype = 'csv'):
-	parameters = {
-		'function': function,
-		'symbol': symbol,
-		'output': output,
-		'datatype': datatype,
-		'apikey': apikey
-	}
 
-	response = requests.get(API_URL, params = parameters)
-	if(response.status_code == 200):
-		cols = pandas.read_csv(StringIO(response.text))
-		cols.insert(0, 'Ticker', symbol)
-		return cols
-	else:
-		raise RequestFailed("Status_code was not 200")
+def single_csv(symbol, apikey, function='TIME_SERIES_DAILY_ADJUSTED', output='compact', datatype='csv'):
+    parameters = {
+        'function': function,
+        'symbol': symbol,
+        'output': output,
+        'datatype': datatype,
+        'apikey': apikey
+    }
+
+    response = requests.get(API_URL, params=parameters)
+    if response.status_code == 200:
+        cols = pandas.read_csv(StringIO(response.text))
+        cols.insert(0, 'Ticker', symbol)
+        return cols
+    else:
+        raise RequestFailed("Status_code was not 200")
+
 
 ticker = input("Input a ticker\n")
 
