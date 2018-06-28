75e50c1010852c68f99938a0f5a0d677ded0f1a8
Zhen <zjsoulsilent@gmail.com>
2018-06-27 21:28:57 -0500

    Create db_wrapper file with basic functionality

---
+++ backend/db_wrapper.py
@@ -0,0 +1,11 @@
+import config
+import pandas
+import sqlite3 as sql
+
+
+def get_ticker_data_between_dates(ticker, start_date, end_date):
+    sql_query = '''SELECT * FROM historical_data WHERE ticker = "%s" AND timestamp
+                   BETWEEN "%s" AND "%s"''' % (ticker, start_date, end_date)
+    ticker_data = pandas.read_sql(sql_query, sql.connect(config.DB_NAME), parse_dates=['timestamp'])
+    return ticker_data
+
