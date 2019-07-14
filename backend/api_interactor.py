72cc50df3d82b51409decaf802c911523f05c9ce
ramasrirama99 <ramasrirama99@gmail.com>
2019-07-14 13:23:48 -0700

    Modify api_interactor to return lists of tuples from methods instead of pandas dataframes

--- backend/api_interactor.py
+++ backend/api_interactor.py
@@ -1,48 +1,46 @@
 import requests
 from io import StringIO
 import pandas
-from backend.fileio import apikey
+from fileio import apikey
+import csv
 
 API_URL = 'https://www.alphavantage.co/query'
 
+def get_data_for_symbol(parameters):
+    response = requests.get(API_URL, params=parameters)
+    if response.status_code == 200:
+        csv_file = StringIO(response.text)
+        tuple_data_list = [tuple(line) for line in csv.reader(csv_file)]
+        return tuple_data_list
+    else:
+        print('Status_code not 200')
+        return None
 
-def get_historical_data_for_ticker(ticker, function='TIME_SERIES_DAILY_ADJUSTED', outputsize='compact'):
+
+def get_historical_data(symbol, outputsize='compact'):
     parameters = {
-        'function': function,
-        'ticker': ticker,
+        'function': 'TIME_SERIES_DAILY_ADJUSTED',
+        'symbol': symbol,
         'outputsize': outputsize,
         'datatype': 'csv',
         'apikey': apikey.API_KEY
     }
 
-    response = requests.get(API_URL, params=parameters)
-    if response.status_code == 200:
-        cols = pandas.read_csv(StringIO(response.text))
-        cols.insert(0, 'Ticker', ticker)
-        return cols
-    else:
-        print('Status_code not 200')
+    get_data_for_symbol(parameters);
 
 
-def get_intraday_data(ticker, function='TIME_SERIES_INTRADAY', outputsize='compact'):
+
+def get_intraday_data(symbol, interval='1min', outputsize='compact'):
     parameters = {
-        'function': function,
-        'ticker': ticker,
+        'function': 'TIME_SERIES_INTRADAY',
+        'symbol': symbol,
+        'interval' : interval,
         'outputsize': outputsize,
         'datatype': 'csv',
         'apikey': apikey.API_KEY
     }
 
-    response = requests.get(API_URL, params=parameters)
-    if response.status_code == 200:
-        cols = pandas.read_csv(StringIO(response.text))
-        cols.insert(0, 'Ticker', ticker)
-        return cols
-    else:
-        print('Status_code not 200')
+    get_data_for_symbol(parameters);
 
-# ticker = input("Input a ticker\n")
-
-# csv_file = get_historical_data_for_ticker(ticker, apikey.API_KEY, outputsize='full')
-
-# print(csv_file)
+symbol = input("Enter a symbol: ")
+get_historical_data(symbol)
