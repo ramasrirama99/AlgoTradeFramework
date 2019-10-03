945d5a1dc4617eaa92528fba570e628fc7b0890c
Brian Tang <byron123t@gmail.com>
2019-10-03 14:55:08 -0500

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
-setup(name='algotaf', version='1.0', packages=find_packages())
+setup(name='algotaf',
+	  version='1.0',
+	  url='https://github.com/ramasrirama99/AlgoTradeFramework',
+	  license='MIT',
+	  install_requires=[
+	  	'psycopg2',
+	  	'pgcopy',
+	  	'requests',
+	  	'schedule'
+	  ],
+	  packages=find_packages())
